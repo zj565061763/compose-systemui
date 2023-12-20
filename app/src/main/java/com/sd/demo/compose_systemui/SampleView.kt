f22b8324ef82a163ce2e88b8ec75923d68e3de6b
zjun <565061763@qq.com>
2023-12-20 23:30:16 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_systemui/SampleView.kt
+++ app/src/main/java/com/sd/demo/compose_systemui/SampleView.kt
@@ -3,9 +3,7 @@ package com.sd.demo.compose_systemui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.sd.demo.compose_systemui.databinding.SampleViewBinding
-import com.sd.lib.compose.systemui.Brightness
 import com.sd.lib.compose.systemui.IStatusBarController
-import com.sd.lib.compose.systemui.fStatusBarBrightnessStack
 
 class SampleView : ComponentActivity() {
     private val _binding by lazy { SampleViewBinding.inflate(layoutInflater) }
@@ -14,19 +12,18 @@ class SampleView : ComponentActivity() {
         IStatusBarController.create(_binding.root)
     }
 
-    private val _light = Brightness.light()
-    private val _dark = Brightness.dark()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(_binding.root)
 
         _binding.btnLight.setOnClickListener {
-            fStatusBarBrightnessStack().add(_light)
+            _statusBarController.isLight = true
         }
+
         _binding.btnDark.setOnClickListener {
-            fStatusBarBrightnessStack().add(_dark)
+            _statusBarController.isLight = false
         }
+
         _binding.btnToggleVisible.setOnClickListener {
             _statusBarController.isVisible = !_statusBarController.isVisible
         }
