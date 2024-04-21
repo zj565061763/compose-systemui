4c9ae0f1623ee82344596702ad878b1e8125424b
zjun <565061763@qq.com>
2024-04-21 20:56:43 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_systemui/CommonUI.kt
+++ app/src/main/java/com/sd/demo/compose_systemui/CommonUI.kt
@@ -6,11 +6,49 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.sd.lib.compose.systemui.ISystemUIController
+
+/**
+ * 明亮度
+ */
+@Composable
+fun BarBrightness(
+    onChange: @Composable (isLight: Boolean) -> Unit,
+) {
+    var isLight by remember { mutableStateOf(false) }
+
+    onChange(isLight)
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(5.dp),
+    ) {
+        Button(
+            onClick = {
+                isLight = true
+            }
+        ) {
+            Text("light")
+        }
+
+        Button(
+            onClick = {
+                isLight = false
+            }
+        ) {
+            Text("dark")
+        }
+    }
+}
 
 /**
  * 颜色
--- app/src/main/java/com/sd/demo/compose_systemui/SampleStatusBar.kt
+++ app/src/main/java/com/sd/demo/compose_systemui/SampleStatusBar.kt
@@ -6,16 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -49,48 +42,19 @@ private fun Content() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(5.dp),
     ) {
-        BarBrightness()
+        // 明亮度
+        BarBrightness { isLight ->
+            if (isLight) {
+                FStatusBarLight()
+            } else {
+                FStatusBarDark()
+            }
+        }
 
         HorizontalDivider()
         BarColor(fStatusBarController())
 
         HorizontalDivider()
         BarVisibility(fStatusBarController())
-    }
-}
-
-/**
- * 明亮度
- */
-@Composable
-private fun BarBrightness() {
-    var isLight by remember { mutableStateOf(false) }
-
-    if (isLight) {
-        FStatusBarLight()
-    } else {
-        FStatusBarDark()
-    }
-
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(5.dp),
-    ) {
-        Button(
-            onClick = {
-                isLight = true
-            }
-        ) {
-            Text("light")
-        }
-
-        Button(
-            onClick = {
-                isLight = false
-            }
-        ) {
-            Text("dark")
-        }
     }
 }
