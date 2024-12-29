7f0251bd34418cdd7a07d6b28fcf3d38a09bf297
zjun <565061763@qq.com>
2024-12-29 10:59:05 +0800

    1.2.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-systemui"
-val libVersion = "1.1.2"
+val libVersion = "1.2.0"
 
 android {
   namespace = "com.sd.lib.compose.systemui"
