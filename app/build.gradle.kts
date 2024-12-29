b37d6e6990902629cc9e4077791e3a5ffc654f67
zjun <565061763@qq.com>
2024-12-29 10:58:29 +0800

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("com.android.application")
-  id("org.jetbrains.kotlin.android")
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -26,6 +26,15 @@ android {
     }
   }
 
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+
   buildFeatures {
     compose = true
     viewBinding = true
@@ -36,11 +45,9 @@ android {
   }
 }
 
-kotlin {
-  jvmToolchain(8)
-}
-
 dependencies {
+  implementation(project(":lib"))
+
   implementation(libs.androidx.compose.foundation)
   implementation(libs.androidx.compose.ui.tooling.preview)
   debugImplementation(libs.androidx.compose.ui.tooling)
@@ -58,6 +65,4 @@ dependencies {
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.test.ext.junit)
   androidTestImplementation(libs.androidx.test.espresso.core)
-
-  implementation(project(":lib"))
 }
